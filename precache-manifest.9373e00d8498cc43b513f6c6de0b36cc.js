93922cae6880b260009c83b06cccdff0d0980d25
Ilnaz Valiev <ilnazv90@gmail.com>
2019-07-24 01:47:55 -0700

    Updates

---
+++ precache-manifest.9373e00d8498cc43b513f6c6de0b36cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc5d4d2296f0836cdeeabff607dcd6b",
+    "url": "/doto/index.html"
+  },
+  {
+    "revision": "174d103d462ca2aa9380",
+    "url": "/doto/static/css/4.a9fd1709.chunk.css"
+  },
+  {
+    "revision": "61f9853b662dd7219530",
+    "url": "/doto/static/css/main.e9e80489.chunk.css"
+  },
+  {
+    "revision": "1a02bd9ba79cec8c799d",
+    "url": "/doto/static/js/2.08127bf3.chunk.js"
+  },
+  {
+    "revision": "4c7b1dce6f7f7089e727",
+    "url": "/doto/static/js/3.f5ca6e5a.chunk.js"
+  },
+  {
+    "revision": "174d103d462ca2aa9380",
+    "url": "/doto/static/js/4.c7e6a989.chunk.js"
+  },
+  {
+    "revision": "61f9853b662dd7219530",
+    "url": "/doto/static/js/main.ead5e76a.chunk.js"
+  },
+  {
+    "revision": "51f8be2d73949a4aba57",
+    "url": "/doto/static/js/runtime~main.1ece29e9.js"
+  }
+]);
