1507d3ccaf9f484578856c9b23644ab4e3db1a88
Ilnaz Valiev <ilnazv90@gmail.com>
2019-07-23 23:40:09 -0700

    files formatted

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App: React.FC = () => {
-  const Dashboard = React.lazy(() => import('./Pages/Dashboard/Dashboard'));
-  return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-        </Switch>
-      </Suspense>
-    </Router>
-  );
+    const Dashboard = React.lazy(() => import('./Pages/Dashboard/Dashboard'));
+    return (
+        <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                </Switch>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
